0a0e9479cd8717c1e5505d7f2ccb7c0a6706280b
ryomenhaider <onlinep680@gmail.com>
2025-07-23 13:38:37 +0500

    subscription added

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,6 @@ import App1 from './App1.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App1 />
+    <script src="//web.webformscr.com/apps/fc3/build/loader.js" async sp-form-id="241920fa04aa3df19cd3893bc85f2bb2c54c9a2a643361164a53127a96f04bf7"></script>
   </StrictMode>,
 )
